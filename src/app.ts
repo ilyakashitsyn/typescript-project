306002ea5f29d2c71ab3db93af902fcbcb03b639
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-06-05 23:05:01 -0300

    some typescript code

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import fs from 'node:fs';
+
+// fs.readFileSync();
+
+let userName: string;
+userName = 'Max';
+console.log(userName);
+
+function add(a: any, b: any) {
+  return a + b;
+}
+
+console.log(add(3, 2));
